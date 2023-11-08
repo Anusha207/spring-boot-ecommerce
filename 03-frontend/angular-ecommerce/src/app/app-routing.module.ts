89750b058de84ea5eb8c54bac44622045b0dd0c0
Anusha <anusha.amuluru10@gmail.com>
2023-11-08 19:16:42 +0800

    update the code

--- 03-frontend/angular-ecommerce/src/app/app-routing.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDeatilsComponent } from './components/cart-deatils/cart-deatils.component';
 
 const routes: Routes = [
+  {path:"cart-details",component:CartDeatilsComponent},
   {path:"products/:id",component:ProductDetailsComponent},
   {path:"search/:keyword",component:ProductsListComponent},
   {path:"category/:id",component:ProductsListComponent},
--- 03-frontend/angular-ecommerce/src/app/components/cart-deatils/cart-deatils.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-deatils/cart-deatils.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-deatils',
   templateUrl: './cart-deatils.component.html',
   styleUrls: ['./cart-deatils.component.css']
 })
-export class CartDeatilsComponent {
+export class CartDeatilsComponent implements OnInit {
+  cartItem:CartItem[]=[];
+  totalPrice:number=0;
+  totalQuantity:number=0;
+  constructor(private cartService:CartService){}
+  ngOnInit(): void {
+    this.listCartdetails();
+    
+  }
+  listCartdetails() {
+    //get a handle to the cart items
+    this.cartItem=this.cartService.cartItem;
+
+    //subscrobe tp the cart total price
+    this.cartService.totalPrice.subscribe(
+      data=>this.totalPrice=data
+    );
+   
+
+    //subscribe to the cart total  qunatity
+    this.cartService.totalQuantity.subscribe(
+      data=>this.totalQuantity=data
+    );
+    //compute cart total price and quantity
+    this.cartService.computeCartTotals();
+  }
 
 }
